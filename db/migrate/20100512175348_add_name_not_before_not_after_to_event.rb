f18f6ea191396ddc935252bbd8f6d58f36348e76
Winston Wolff <winston@carbonfive.com>
2010-05-12 15:54:02 -0700

    Fix migration

--- db/migrate/20100512175348_add_name_not_before_not_after_to_event.rb
+++ db/migrate/20100512175348_add_name_not_before_not_after_to_event.rb
@@ -1,7 +1,7 @@
 class AddNameNotBeforeNotAfterToEvent < ActiveRecord::Migration
   def self.up
-    remove_column :events, :not_before
-    remove_column :events, :not_after
+    remove_column :events, :start_time
+    remove_column :events, :end_time
     add_column :events, :not_before, :datetime
     add_column :events, :not_after, :datetime
     add_column :events, :admin_password, :string
@@ -11,7 +11,7 @@ class AddNameNotBeforeNotAfterToEvent < ActiveRecord::Migration
     remove_column :events, :not_after
     remove_column :events, :not_before
     remove_column :events, :admin_password
-    add_column :events, :not_before, :date
-    add_column :events, :not_after, :date
+    add_column :events, :start_time, :date
+    add_column :events, :end_time, :date
   end
 end
