b7f56a6724cc6edd6050893f71ee623d2b00284d
Erik Ingenito <erik@carbonive.com>
2010-05-24 10:44:08 -0700

    Deploy to cold-lightning-71

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rvm system
-git --git-dir=/home/teamcity/.BuildServer/system/caches/git/git-B1D9E8DE.git push git@heroku.com:warm-beach-63.git
-heroku rake db:migrate db:seed --app warm-beach-63
+git --git-dir=/home/teamcity/.BuildServer/system/caches/git/git-B1D9E8DE.git push git@heroku.com:cold-lightning-71.git
+heroku rake db:migrate db:seed --app cold-lightning-71
