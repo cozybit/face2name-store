5863d3af696aa1dbab1258d75406d98254170713
Winston Wolff <winston@carbonfive.com>
2010-05-26 18:39:24 -0700

    An error in integration test will open the offending page in a browser.

--- test/integration/store_to_server_test.rb
+++ test/integration/store_to_server_test.rb
@@ -2,6 +2,7 @@ require 'test/unit'
 require 'rubygems'
 require 'mechanize'
 require 'time'
+require 'tmpdir'
 
 # See mechanize docs at:
 #   http://mechanize.rubyforge.org/mechanize/
@@ -52,64 +53,74 @@ class StoreToServerTest < Test::Unit::TestCase
 
     browser = Mechanize.new
 
-    # On login page, Sign in. Should go to welcome page.
-    login_page = browser.get(STORE_URL)
-    assert login_page != nil
-    welcome_page = login_page.form_with( :action => "/users/sign_in") do |f|
-      assert f != nil, 'Could not find Login form.'
-      puts "form=#{f}"
-      f["user[email]"] = "unlimited@test.com"
-      f["user[password]"] = "simple"
-    end.click_button
+    begin
+      # On login page, Sign in. Should go to welcome page.
+      login_page = browser.get(STORE_URL)
+      assert login_page != nil
+      welcome_page = login_page.form_with( :action => "/users/sign_in") do |f|
+        assert f != nil, 'Could not find Login form.'
+        puts "form=#{f}"
+        f["user[email]"] = "unlimited@test.com"
+        f["user[password]"] = "simple"
+      end.click_button
 
-    # On welcome page, click on New Event
-    new_event_link = welcome_page.link_with(:href => '/events/new')
-    assert nil != new_event_link, 'Could not find new event link.'
-    new_event_page = new_event_link.click
-    assert nil != new_event_page
+      # On welcome page, click on New Event
+      new_event_link = welcome_page.link_with(:href => '/events/new')
+      assert nil != new_event_link, 'Could not find new event link.'
+      new_event_page = new_event_link.click
+      assert nil != new_event_page
 
-    # Fill in New Event form
-    timestamp = Time.now().strftime('%b%d %H%M')
-    not_before = Date.today() + rand(90)+1
-    not_after = not_before + rand(9)+1
-    the_event_page = new_event_page.form_with( :action=>"/events") do |f|
-      assert nil != f
-      f["event[name]"]="Automated Integration Cruise "+timestamp
+      # Fill in New Event form
+      timestamp = Time.now().strftime('%b%d %H%M')
+      not_before = Date.today() + rand(90)+1
+      not_after = not_before + rand(9)+1
+      the_event_page = new_event_page.form_with( :action=>"/events") do |f|
+        assert nil != f
+        f["event[name]"]="Automated Integration Cruise "+timestamp
 
-      f["event[not_before(1i)]"]=not_before.year
-      f["event[not_before(2i)]"]=not_before.mon
-      f["event[not_before(3i)]"]=not_before.day
+        f["event[not_before]"]=not_before.strftime('%b %d, %Y')
+        f["event[not_after]"]=not_after.strftime('%b %d, %Y')
+        f["event[admin_password]"]="TEST_MASTER_PASS"
+      end.click_button
 
-      f["event[not_after(1i)]"]=not_after.year
-      f["event[not_after(2i)]"]=not_after.mon
-      f["event[not_after(3i)]"]=not_after.day
-      f["event[admin_password]"]="TEST_MASTER_PASS"
-    end.click_button
+      # Download the configuration file
+      assert the_event_page != nil
+      download_configuration_btn = the_event_page.link_with(:text=>"Download Configuration")
+      assert download_configuration_btn != nil, 'Could not find Download Configuration button. Is the user an unlimited user? Remember not the test db but the currently running store.'
+      config_file = download_configuration_btn.click
+  #    yes_download_btn = confirmation_dialog.button_with(:text=>"Yes - Download")
+  #    config_file = yes_download_btn.click
+      assert config_file.body.length > 0
+      assert config_file.response['content-type'] == 'application/octet-stream'
+      config_tmp_filename = File.join( Dir.tmpdir, config_file.filename )
+      File.open(config_tmp_filename, 'w') do |file|
+        file.write(config_file.body)
+      end
 
-    # Download the configuration file
-    assert the_event_page != nil
-    download_configuration_btn = the_event_page.link_with(:text=>"Download Configuration")
-    assert download_configuration_btn != nil, 'Could not find Download Configuration button. Is the user an unlimited user? Remember not the test db but the currently running store.'
-    config_file = download_configuration_btn.click
-#    yes_download_btn = confirmation_dialog.button_with(:text=>"Yes - Download")
-#    config_file = yes_download_btn.click
-    assert config_file.body.length > 0
-    assert config_file.response['content-type'] == 'application/octet-stream'
-    config_tmp_filename = File.join( Dir.tmpdir, config_file.filename )
-    File.open(config_tmp_filename, 'w') do |file|
-      file.write(config_file.body)
+
+      #
+      # Import configuration file to f2n server
+      #
+      import_page = browser.get( SERVER_IMPORT_URL )
+      import_form = import_page.form_with(:action=>"/config/upload")
+      import_form.file_uploads[0].file_name = config_tmp_filename
+      result_json = import_form.click_button
+      assert result_json.body.include?("\"success\":true")  # should return true
+    rescue Mechanize::ResponseCodeError => exc
+      puts "!!! test_buy_and_import_configuration: Exception body=#{exc}"
+      openStringInBrowser( exc.page.body )
+      throw exc
     end
-
-
-    #
-    # Import configuration file to f2n server
-    #
-    import_page = browser.get( SERVER_IMPORT_URL )
-    import_form = import_page.form_with(:action=>"/config/upload")
-    import_form.file_uploads[0].file_name = config_tmp_filename
-    result_json = import_form.click_button
-    assert result_json.body.include?("\"success\":true")  # should return true
   end
+end
+
+def openStringInBrowser( htmlStr )
+  tmp_filename = File.join( Dir.tmpdir, 'temp.html')
+  tmp = File.open( tmp_filename, 'w')
+  tmp.write( htmlStr )
+  tmp.close()
+  puts "!!! Error body stored here: #{tmp_filename}"
+  system( "open file://#{tmp.path}" )
 end
 
 if __FILE__ == $0
