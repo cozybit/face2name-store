e4df36d966a2bf20c2d14210dc90b35111b7a4fd
Erik Ingenito <erik@carbonive.com>
2010-06-02 08:54:30 -0700

    fixed cache directive in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,29 +1,48 @@
 F2nstore::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
-  # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = false
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  # For nginx:
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # If you have no front-end server that supports something like X-Sendfile,
+  # just comment this out and Rails will serve the files
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Disable Rails's static asset server
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = true
+
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
-#  config.action_mailer.smtp_settings = {
-#      :address => "smtp.carbonfive.com",
-#      :port => 25,
-#      :enable_starttls_auto => false
-#  }
-
-#  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
+  config.action_mailer.raise_delivery_errors = true
 end
 
 F2N.deep_merge!({
